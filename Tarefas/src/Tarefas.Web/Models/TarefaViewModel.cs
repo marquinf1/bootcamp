483d34a41273970632e42e69d290bc62809f159a
marquinf1 <marcos.ferraz@ufvjm.edu.br>
2023-01-24 16:28:51 -0300

    Versao até form

--- Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
+++ Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -1,5 +1,13 @@
+using System.ComponentModel;
+
 namespace Tarefas.Web.Models;
 public class TarefaViewModel {
+
+    [DisplayName("Titulo")]
+
     public string Titulo {get;set;}
+
+    [DisplayName("Descrição")]
+
     public string Descricao {get;set;}
 }
