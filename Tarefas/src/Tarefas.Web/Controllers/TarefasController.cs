7ef58900891046547736c1d1f5c60bd8a97a7b93
Patrick <patrickjhonsom@gmail.com>
2023-01-25 13:23:19 -0300

    Adicionando Git Ignore

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Tarefas.Web.Controllers;
 
-public class TarefasController : Controller {
+public class TarefasController : Controller 
+{
     public IActionResult Create () {
         return View();
     }
