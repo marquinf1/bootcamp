c3b3490b96c714d0b2d19152dced495941a26a1b
marquinf1 <marcos.ferraz@ufvjm.edu.br>
2023-01-25 15:43:06 -0300

    Página Details

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -6,18 +6,32 @@ namespace Tarefas.Web.Controllers;
 
 public class TarefasController : Controller 
 {
-    public IActionResult Create () {
+    public List<TarefaViewModel> listaDeTarefas {get;set;}
+
+    public TarefasController() 
+    {
+        listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel() { Id = 1, Titulo = "Escovar os dentes", Descricao = "Usar Sensodine de manhã e não de noite"},
+            new TarefaViewModel() { Id = 2, Titulo = "Arrumar a cama", Descricao = "Usar Inteligência"},
+            new TarefaViewModel() { Id = 3, Titulo = "Colocar lixo pra fora", Descricao = "Usar Luvas"},
+        };
+    } 
+
+    public IActionResult Create () 
+    {
         return View();
     }
 
     public IActionResult Index () 
     {
-        var listaDeTarefas = new List<TarefaViewModel>()
-        {
-            new TarefaViewModel() { Titulo = "Escovar os dentes", Descricao = "Usar Sensodine de manhã e não de noite"},
-            new TarefaViewModel() { Titulo = "Arrumar a cama", Descricao = "Usar inteligencia"},
-            new TarefaViewModel() { Titulo = "Colocar lixo pra fora", Descricao = "Usar Luvas"},
-        };
         return View(listaDeTarefas);
     }
+
+    public IActionResult Details (int id)
+    {
+        var tarefa = listaDeTarefas.Find(tarefa => tarefa.Id == id);
+        return View(tarefa);
+    }
+
 }
--- Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
+++ Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -3,6 +3,8 @@ using System.ComponentModel;
 namespace Tarefas.Web.Models;
 public class TarefaViewModel {
 
+    public int Id {get;set;}
+
     [DisplayName("Titulo")]
 
     public string Titulo {get;set;}
