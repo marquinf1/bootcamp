39e7a6a480a568f95cdd1afc91cf7bbb804f8e49
marquinf1 <marcos.ferraz@ufvjm.edu.br>
2023-01-25 14:11:20 -0300

    Dia 2

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers;
 
@@ -7,5 +8,16 @@ public class TarefasController : Controller
 {
     public IActionResult Create () {
         return View();
+    }
+
+    public IActionResult Index () 
+    {
+        var listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel() { Titulo = "Escovar os dentes", Descricao = "Usar Sensodine de manhã e não de noite"},
+            new TarefaViewModel() { Titulo = "Arrumar a cama", Descricao = "Usar inteligencia"},
+            new TarefaViewModel() { Titulo = "Colocar lixo pra fora", Descricao = "Usar Luvas"},
+        };
+        return View(listaDeTarefas);
     }
 }
